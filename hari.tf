9c795e61a22b6a0df2481e2eccf9193523fa3493
hariprasad8452 <116292804+hariprasad8452@users.noreply.github.com>
2023-03-21 20:13:31 +0530

    Update hari.tf

--- hari.tf
+++ hari.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIA2KQG3KB2OAUOKGUT"
+secret_key = "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"
 }
 resource "aws_instance" "one" {
   ami           = "ami-0d81306eddc614a45"
